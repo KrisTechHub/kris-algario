4e0e5cde32f9dfc7fe71dbead70e2f63ef3f5ee7
kristechhub <kmj.algario@gmail.com>
2023-11-18 17:39:45 +0800

    finalized about page

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -2,14 +2,14 @@ import AboutInfo from "./AboutInfo";
 
 export default function About() {
     return (
-        <div className="container mx-auto">
+        <div className="container mx-auto py-28">
             <div className="flex-row mx-auto">
                 <div className="flex flex-col sm:flex-row xl:px-[50px]">
-                    <div className="order-2 sm:order-1 items-center w-full lg:w-1/2">
+                    <div className="order-1 sm:order-1 items-center w-full lg:w-1/2">
                         <img src="./src/assets/about-portrait.svg" alt="home portrait" />
                     </div>
 
-                    <div className="order-1 sm:order-2 flex items-center w-full lg:w-1/2 ">
+                    <div className="order-2 sm:order-2 flex items-center w-full lg:w-1/2 ">
                         <AboutInfo/>
                     </div>
                 </div>
--- src/Components/About/AboutInfo.jsx
+++ src/Components/About/AboutInfo.jsx
@@ -2,21 +2,21 @@ export default function AboutInfo() {
     return (
         <div>
             <div className="flex-row items-center">
-                <h3 className="font-JosefinBold">ABOUT ME</h3>
+                <h3 className="font-JosefinBold pb-1">ABOUT ME</h3>
             </div>
 
             <div className="flex-row items-center">
-                <h1 className="font-JosefinBold text-4xl">Why hire me for your project?</h1>
+                <h1 className="font-JosefinBold text-4xl pb-1 text-mypurple-900">Why hire me for your project?</h1>
             </div>
 
             <div className="flex-row items-center">
-                <p className="font-RalewayMedium">
-                    As a self-taught front-end developer, I've gained proficiency in React development, with growing experience. My skill set includes JavaScript, CSS Frameworks, and MERN Stack. Creating visually appealing, user-friendly, and responsive web pages and applications is where my passion lies. My motivation and enthusiasm are driven by this passion, and my commitment to continuous learning keeps me humble and open to new knowledge. 
+                <p className="font-RalewayMedium pb-1 text-gray-800">
+                    As a self-taught front-end developer, I have gained proficiency in React development with growing experience. My skill set includes JavaScript, CSS Frameworks, and MERN Stack. Creating visually appealing, user-friendly, and responsive web pages and applications is where my passion lies. My motivation and enthusiasm are driven by this passion, and my commitment to continuous learning keeps me humble and open to new knowledge. 
                 </p>
             </div>
 
-            <div className="flex-row items-center">
-                <div className="flex flex-wrap gap-3">
+            <div className="flex-row sm:justify-content">
+                <div className="flex flex-wrap gap-3 pt-2">
                         <div className="rounded-full bg-mypurple-900">
                             <img className="w-1/7 p-2" src="./src/icons/html5.svg" alt="html5" />  
                         </div>
@@ -38,8 +38,13 @@ export default function AboutInfo() {
                         <div className="rounded-full bg-mypurple-900">
                             <img className="w-1/7 p-2" src="./src/icons/react.svg" alt="react" />  
                         </div>
-             </div>
+                </div>
 
+                <a href='' download='' target="_blank" rel="noopener noreferrer">
+                    <button className="mt-4 font-RalewayMedium tracking-wider text-white text-sm rounded-lg p-2 bg-gradient-to-r from-mypink-600 to-mypurple-400 hover:from-mypink-500 hover:to-mypurple-200">
+                        DOWNLOAD CV
+                    </button>
+                </a>
             </div>
 
             <div className="flex-row items-center">
