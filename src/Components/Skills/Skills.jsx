de9c6a266990e2c2d0d792691a3610f1da83e8fc
kristechhub <kmj.algario@gmail.com>
2023-11-26 00:36:30 +0800

    fixing skills page

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -3,24 +3,28 @@ import { Carousel, IconButton } from '@material-tailwind/react';
 export default function Skills() {
     return (
         <div className='container mx-auto py-28'>
+            <div className='text-center'>
+                <h3 className="font-JosefinBold">SKILLS</h3>
+                <h1 className="justify-center font-JosefinBold text-[30px] lg:text-[45px] text-mypink-800">W<span className="text-[22px] lg:text-[30px]">hat I do</span></h1>
+            </div>
             <Carousel className='rounded-xl' prevArrow={({ handlePrev }) => (
-                <IconButton variant='text' color='white' size='lg' onClick={handlePrev} className='!absolute top-2/4 left-4 -translate-y-2/4'>
-                    <svg xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={2}
-                        stroke="currentColor"
-                        className="h-6 w-6">
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
-                        />
-                    </svg>
-                </IconButton>
-            )}
+                <IconButton onClick={handlePrev} className='!absolute top-1/2 left-4 -translate-y-2/4 bg-transparent text-blue-gray-400'>
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={2}
+                            stroke="currentColor"
+                            className="h-6 w-6">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+                            />
+                        </svg>
+                    </IconButton>
+                )}
                 nextArrow={({ handleNext }) => (
-                    <IconButton variant='text' color='white' size='lg' onClick={handleNext} className='!absolute top-2/4 !right-4 -translate-y-2/4'>
+                    <IconButton onClick={handleNext} className='!absolute top-1/2 !right-4 -translate-y-2/4 bg-transparent text-blue-gray-400'>
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             fill="none"
@@ -38,13 +42,66 @@ export default function Skills() {
                     </IconButton>
                 )}
             >
-                <h1>slide 1</h1>
-                <img  src="./src/icons/Design 1.svg" alt="bg design" />
 
-                <h1>slide 2</h1>
-                <h1>slide 3</h1>
+                <div className='flex-row'>
+                    <div className='flex flex-wrap gap-7 px-32 text-center justify-center my-8'>
+                        <div className='bg-mypink-900 rounded-lg text-center'>
+                            <img className='h-28  p-5 mb-[-10px]' src="./src/icons/html5.svg" alt="" />
+                            <h6 className='pb-4 font-JosefinSemibold text-blue-gray-50'>HTML5</h6>
+                        </div>
+                        <div className='bg-mypink-900 rounded-lg text-center'>
+                            <img className='h-28  p-5 mb-[-10px]' src="./src/icons/css3.svg" alt="" />
+                            <h6 className='pb-4 font-JosefinSemibold text-blue-gray-50'>CSS3</h6>
+                        </div>
+                        <div className='bg-mypink-900 rounded-lg text-center'>
+                            <img className='h-28  p-5 mb-[-10px]' src="./src/icons/JS.svg" alt="" />
+                            <h6 className='pb-4 font-JosefinSemibold text-blue-gray-50'>JS</h6>
+                        </div>
+                        <div className='bg-mypink-900 rounded-lg text-center'>
+                            <img className='h-28  p-5 mb-[-10px]' src="./src/icons/SASS.svg" alt="" />
+                            <h6 className='pb-4 font-JosefinSemibold text-blue-gray-50'>SASS</h6>
+                        </div>
+                        <div className='bg-mypink-900 rounded-lg text-center'>
+                            <img className='h-28  p-5 mb-[-10px]' src="./src/icons/GIT.svg" alt="" />
+                            <h6 className='pb-4 font-JosefinSemibold text-blue-gray-50'>Git/Github</h6>
+                        </div>
+                    </div>
 
 
+
+                    <div className='flex flex-wrap gap-5 px-32 text-center justify-center my-8'>
+                        <div className='bg-mypink-900 rounded-lg text-center'>
+                            <img className='h-28  p-5 mb-[-10px]' src="./src/icons/html5.svg" alt="" />
+                            <h6 className='pb-2 font-JosefinSemibold text-blue-gray-50'>HTML5</h6>
+                        </div>
+
+                        <div className='bg-mypink-800 text-center'>
+                            <img className='h-24  p-3' src="./src/icons/css3.svg" alt="" />
+                            <h6>HTML5</h6>
+                        </div>
+                    </div>
+
+
+
+                    <div className='flex flex-wrap gap-5 px-32 text-center justify-center my-8'>
+                        <div className='bg-mypink-900 rounded-lg text-center'>
+                            <img className='h-28  p-5 mb-[-10px]' src="./src/icons/html5.svg" alt="" />
+                            <h6 className='pb-2 font-JosefinSemibold text-blue-gray-50'>HTML5</h6>
+                        </div>
+
+                        <div className='bg-mypink-800 text-center'>
+                            <img className='h-24  p-3' src="./src/icons/css3.svg" alt="" />
+                            <h6>HTML5</h6>
+                        </div>
+                    </div>
+                </div>
+                
+                <div className='bg-blue-800'>
+                    <div className='bg-mypurple-800'>
+                        <img className='h-20  p-3' src="./src/icons/css3.svg" alt="" />
+                        <h6>HTML5</h6>
+                    </div>
+                </div>
             </Carousel>
         </div>
     );
